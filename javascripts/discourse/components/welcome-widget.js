b2a9813d7c7b32d2559cefc6b7487f734c1f3572
David Taylor <david@taylorhq.com>
2024-03-27 16:18:07 +0000

    DEV: Update linting (#17)

--- javascripts/discourse/components/welcome-widget.js
+++ javascripts/discourse/components/welcome-widget.js
@@ -1,7 +1,7 @@
 import Component from "@ember/component";
-import discourseComputed from "discourse-common/utils/decorators";
-import { inject as service } from "@ember/service";
+import { service } from "@ember/service";
 import { defaultHomepage } from "discourse/lib/utilities";
+import discourseComputed from "discourse-common/utils/decorators";
 
 export default Component.extend({
   tagName: "",
